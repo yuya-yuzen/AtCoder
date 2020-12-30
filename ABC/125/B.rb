d02ed9de8206499a883b15dac666ba37a8de88e1
yuya <blue_sky_sora8@yahoo.co.jp>
2020-12-30 16:36:17 +0900

    fix(abc125): fix B

--- ABC/125/B.rb
+++ ABC/125/B.rb
@@ -1,23 +1,12 @@
-N  個の宝石があり、i 番目の宝石の価値は Vi です。
-あなたはこれらの宝石の中からいくつかを選んで手に入れます。
-このとき、1 つも選ばなくとも、全て選んでも構いません。
-ただし、i 番目の宝石を手に入れる場合コスト Ci を支払わなければいけません。
-手に入れた宝石の価値の合計を X、支払ったコストの合計を Y とします。
-X−Y の最大値を求めてください。
-
 n = gets.to_i
+v = gets.chomp.split.map(&:to_i)
+c = gets.chomp.split.map(&:to_i)
 
-v = gets.chomp.split
+cost_performances = [0]
 n.times do |i|
-    v[i] = v[i].to_i
+    cost_performance = v[i] - c[i]
+    if cost_performance > 0
+        cost_performances << cost_performance
+    end
 end
-
-c = gets.chomp.split
-n.times do |i|
-    c[i] = c[i].to_i
-end
-
-values = [0]
-
-(1..n-1).each do |i|
-    
+puts cost_performances.sum
