17a27c3151c76e8c7e941e4fd4cc38bbc77129ee
yuya <blue_sky_sora8@yahoo.co.jp>
2021-01-10 09:18:38 +0900

    fix(abc044): fix C

--- ABC/044/C.rb
+++ ABC/044/C.rb
@@ -1,62 +1,21 @@
-na = gets.chomp.split
-n = na[0].to_i
-a = na[1].to_i
+# 部分点の解法
 
-x = gets.chomp.split
-n.times do |i|
-    x[i] = x[i].to_i
-end
-x.sort!
+n, a = gets.split.map(&:to_i)
+x = gets.split.map(&:to_i)
 
 count = 0
-
-if !(x.last < a || x.first > a)
-    (1..n).each do |i|
-        sum_check = 0
-
-        i.times do |j|
-            sum_check += x[n-j-1]
-        end
-
-        next if sum_check < a
-
-        x_c = x.combination(i).to_a
-
-        x_c.each do |combi|
-            sum = 0.0
-
-            combi.each do |num|
-                sum += num
-            end
-
-            ave = sum / combi.length
-            count += 1 if ave == a
+(0..(2 ** n - 1)).each do |i|
+    flag = sprintf('%0*b', n, i)
+    sum = 0
+    card_count = 0
+    n.times do |j|
+        if flag[j] == '1'
+            sum += x[j]
+            card_count += 1
         end
     end
+    ave = 1.0 * sum / card_count
+    count += 1 if ave == a
 end
 
 puts count
-
-# na = gets.chomp.split
-# n = na[0].to_i
-# a = na[1].to_i
-
-# x = gets.chomp.split
-# y = []
-# n.times do |i|
-#     y << x[i].to_i - a
-# end
-
-# y.select! do |num|
-#     num == 0
-# end
-
-# count = 0
-# if y.length > 0
-#     (1..y.length).each do |i|
-#         y_c = y.combination(i).to_a
-#         count += y_c.length
-#     end
-# end
-
-# puts count
